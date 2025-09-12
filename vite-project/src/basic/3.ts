974bc8e765da62af9f1cf7620d142ff6b60e05ec
RomaChernyak <118313101+RomaChernyak@users.noreply.github.com>
2025-09-12 17:37:44 +0300

    small changes

--- vite-project/src/basic/3.ts
+++ vite-project/src/basic/3.ts
@@ -1,6 +1,6 @@
 // Створіть змінну, яка може містити або рядок, або число (union type)? Також, оголосіть змінну, яка може містити лише одне з двох можливих рядкових значень: 'enable' або 'disable' (literal type).
 
-type SwitchStatus = 'enable' | 'disable';
+type literalType = 'enable' | 'disable';
 
 let union: string | number;
 let literal: SwitchStatus;
